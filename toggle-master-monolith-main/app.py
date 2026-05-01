34b9539ebe75fb7812a11b271556e71680dc55f9
Lucas Santos <113461420+caslulucas@users.noreply.github.com>
2026-04-30 21:55:24 -0300

    Update app.py

--- toggle-master-monolith-main/app.py
+++ toggle-master-monolith-main/app.py
@@ -10,13 +10,15 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_SSLMODE = os.getenv("DB_SSLMODE", "require")
 
 def get_db_connection():
     conn = psycopg2.connect(
         host=DB_HOST,
         database=DB_NAME,
         user=DB_USER,
-        password=DB_PASSWORD
+        password=DB_PASSWORD,
+        sslmode=DB_SSLMODE
     )
     return conn
 
